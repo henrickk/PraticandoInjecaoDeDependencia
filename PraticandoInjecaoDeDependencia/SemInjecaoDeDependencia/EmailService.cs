a32eb717cc083bb0d100d817617e8fc8b5804dbf
Henrick Adrian <henrick.adrian4@gmail.com>
2025-08-21 23:31:25 -0300

    Adicionar arquivos de projeto.

---
+++ PraticandoInjecaoDeDependencia/SemInjecaoDeDependencia/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticandoInjecaoDeDependencia.SemInjecaoDeDependencia
+{
+    public class EmailService
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Enviando email: {message}");
+        }
+    }
+}
---
+++ PraticandoInjecaoDeDependencia/SemInjecaoDeDependencia/Notificacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticandoInjecaoDeDependencia.SemInjecaoDeDependencia
+{
+    public class Notificacao
+    {
+        private EmailService _emailService = new EmailService(); // acoplamento forte
+
+        public void SendNotification(string message)
+        {
+            _emailService.Send(message);
+        }
+    }
+}
