0c1c8e5c032b8c24201949e5f70d2680c8d24a3f
Henrick Adrian <henrick.adrian4@gmail.com>
2025-09-02 20:58:51 -0300

    .

--- PraticandoInjecaoDeDependencia/Pratica2/ComInjecaoDeDependencia2/Services/RelatorioService.cs
+++ PraticandoInjecaoDeDependencia/Pratica2/ComInjecaoDeDependencia2/Services/RelatorioService.cs
@@ -1,11 +1,4 @@
 ﻿using PraticandoInjecaoDeDependencia.Pratica2.ComInjecaoDeDependencia2.Interfaces;
-using PraticandoInjecaoDeDependencia.Pratica2.ComInjecaoDeDependencia2.Models;
-using PraticandoInjecaoDeDependencia.Pratica2.ComInjecaoDeDependencia2.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PraticandoInjecaoDeDependencia.Pratica2.ComInjecaoDeDependencia2.Services
 {
